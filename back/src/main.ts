564566cffd741ceecc189c6503339e29af8df0d1
dpclfk <lee@192.168.nate.com>
2024-09-12 22:42:52 +0900

    action6

--- back/src/main.ts
+++ back/src/main.ts
@@ -41,6 +41,6 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(3080);
 }
 bootstrap();
--- front/src/lib/server.ts
+++ front/src/lib/server.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const serverbase = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "http://localhost:3080/api/",
   withCredentials: true,
 });
 
